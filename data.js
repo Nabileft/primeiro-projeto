3957cb8b8d3ba3138ff5d9afa73d1f1bc360e46a
Nabil Emillul Fata <nabilemillulfata@gmail.com>
2024-08-11 01:18:17 +0700

    Update data.js

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ function func() {
     var myname = document.getElementById("nama").value.toLowerCase();
     var mydate = document.getElementById("tanggal").value;
 
-    var expectedDate = "2024-08-10"; // Ganti dengan tanggal yang diharapkan
+    var expectedDate = "2024-08-11"; // Ganti dengan tanggal yang diharapkan
 
     if (mydate === expectedDate) {
         localStorage.setItem("name", myname); // Simpan nama di localStorage
